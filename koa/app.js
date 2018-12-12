b84cb7ec2ee6fdc643d196d2e1ebf12881943cea
Slav <shutrow@rambler.ru>
2018-12-12 17:13:27 +0300

    webinar koa

--- koa/app.js
+++ koa/app.js
@@ -1,37 +1,36 @@
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
-const static = require('koa-static');
-const fs = require('fs');
-const session = require('koa-session');
-const config = require('./config/config.json');
+const static = require("koa-static");
+const fs = require("fs");
+const session = require("koa-session");
+const config = require("./config/config.json");
 
-const Pug = require('koa-pug');
+const Pug = require("koa-pug");
 const pug = new Pug({
-  viewPath: './views',
+  viewPath: "./views",
   pretty: false,
-  basedir: './views',
+  basedir: "./views",
   noCache: true,
-  app: app, // equals to pug.use(app) and app.use(pug.middleware)
+  app: app // equals to pug.use(app) and app.use(pug.middleware)
 });
 
-const errorHandler = require('./libs/error');
-
-app.use(static('./public'));
-
-app.use(errorHandler);
-app.on('error', (err, ctx) => {
-  ctx.render('error', {
-    status: ctx.response.status,
-    error: ctx.response.message,
-  });
-});
-
-const router = require('./routes');
+app.use(static("./public"));
+const router = require("./routes");
 
 app
   .use(session(config.session, app))
   .use(router.routes())
   .use(router.allowedMethods());
+
+const errorHandler = require("./libs/error");
+
+app.use(errorHandler);
+app.on("error", (err, ctx) => {
+  ctx.render("error", {
+    status: ctx.response.status,
+    error: ctx.response.message
+  });
+});
 
 app.listen(3000, () => {
   if (!fs.existsSync(config.upload)) {
